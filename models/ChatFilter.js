e95d719c2664e578a366e96f281839a544630c4e
Fuechschen <me@fuechschen.org>
2016-08-15 19:35:13 +0200

    add mode type

--- models/ChatFilter.js
+++ models/ChatFilter.js
@@ -3,6 +3,7 @@ module.exports = function (sequelize, Sequelize) {
         enabled:{type:Sequelize.BOOLEAN,defaultValue:false},
         ignore_role:{type:Sequelize.STRING(32)},
         links:{type:Sequelize.BOOLEAN,defaultValue:true},
+        links_mode:{type:Sequelize.ENUM('whitelist','blacklist')},
         spam:{type:Sequelize.BOOLEAN,defaultValue:true},
         word_blacklist:{type:Sequelize.BOOLEAN,defaultValue:true}
     }, {
