f86ad2dfb68451d10646dd8376ce7a33e4f5724e
Fuechschen <me@fuechschen.org>
2016-08-20 01:01:55 +0200

    more fields for github feeds

--- models/GithubFeed.js
+++ models/GithubFeed.js
@@ -1,6 +1,11 @@
 module.exports = function (sequelize, Sequelize) {
     return sequelize.define('GithubFeed', {
-        id: {type: Sequelize.STRING(14), unique: true, primaryKey:true}
+        id: {type: Sequelize.STRING(14), unique: true, primaryKey:true},
+        last_event:{type:Sequelize.STRING(191)},
+        repository_name:{type:Sequelize.STRING(191)},
+        repository_id:{type:Sequelize.STRING(32)},
+        repository_owner:{type:Sequelize.STRING(191)},
+        repository_owner_id:{type:Sequelize.STING(32)}
     }, {
         underscored: true,
         tableName: 'githubfeeds'
