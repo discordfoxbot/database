d976ddcd471bf656b7e079a4cd905f29d6b3d7b0
Fuechschen <me@fuechschen.org>
2016-08-29 22:36:16 +0200

    key

--- models/Token.js
+++ models/Token.js
@@ -1,5 +1,6 @@
 module.exports = function (sequelize, Sequelize) {
     return sequelize.define('Token', {
+        id: {type: Sequelize.INTEGER.UNSIGNED, allowNull: False, autoIncrement: true, primaryKey: true},
         token: {type: Sequelize.STRING(255), unique: true},
         type: {type: Sequelize.ENUM('user', 'system'), defaultValue: 'user'},
         expires: {type: Sequelize.DATE, defaultValue: null, allowNull: true}
